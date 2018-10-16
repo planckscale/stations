dc9f5e376bd83933c156de5f6ec9033e9837d085
planckscale <sje56805@yahoo.com>
2018-10-16 16:08:48 -0500

    Initial gradle structure and dependencies TODO swagger dependency

---
+++ src/main/java/com/home/stations/stations/StationsApplication.java
@@ -0,0 +1,12 @@
+package com.home.stations.stations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationsApplication.class, args);
+	}
+}
